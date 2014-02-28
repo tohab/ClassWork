9a0dbf264bfc64cc1374e59090c67cf82c0f8ebc
Kent Collins <kent.r.collins@gmail.com>
2014-02-28 10:40:58 -0500

    Hello World Test
    A class for students to practice with

---
+++ HelloStudents.java
@@ -0,0 +1,13 @@
+/*
+*  HelloStudents.java
+*
+*  A Test to see whether you can clone and run a program
+*
+*/
+
+public static void main(String[] args) {
+
+  // Modify the following to add "from <your name>" and submit a pull request
+  System.out.println("Hello, World");
+  
+}
