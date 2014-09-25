97daa504bd91a03d244b677bb1d7f6a220a087e6
Kent Collins <kent.r.collins@gmail.com>
2014-09-25 07:20:25 +0100

    Create TemperatureChanger.java

---
+++ TemperatureChanger.java
@@ -0,0 +1,20 @@
+/*
+*  Convert user input in the format xxxF or yyyC from Celsius to Farenheit
+*
+*  @author Kent Collins
+*  @version 25 September, 2014
+**/
+
+import java.util.Scanner;
+
+public class TemperatureChanger {
+
+  public static void main (String[] args) {
+    // TODO - scan for input in the format 175F or 18C 
+    // TODO - perform correct conversion
+    // TODO - print out results to the screen
+  }
+  
+}
+
+
